fab5ef3bf904dfbc4bbdf79bd46152079aa59ce5
unknown <freitascontato2004@gmail.com>
2022-09-04 22:54:56 -0300

    Resolução do exercício Musica

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.WriteLine("Royalty - Egzod, Maestro Chives");
+        Console.ResetColor();
+        Console.WriteLine();
+        
+        Console.WriteLine("Say I'm cold hearted");
+        Console.WriteLine("But I'm just getting started");
+        Console.WriteLine("Got my eyes on the the target");
+        Console.WriteLine("Now, now");
+        Console.WriteLine();
+
+        Console.WriteLine("Oooh, oooh, oooh");
+        Console.WriteLine("Till the battles won");
+        Console.WriteLine("Till kingdom come");
+        Console.WriteLine("Oooh, oooh, oooh");
+        Console.WriteLine("I'll never run");
+        Console.WriteLine();
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Best to give me your loyalty");
+        Console.WriteLine("Cause I'm taking the world you'll see");
+        Console.WriteLine("They'll be calling me, calling me");
+        Console.WriteLine("They'll be calling me royalty");
+        Console.WriteLine("Best to give me your loyalty");
+        Console.WriteLine("Cause I'm taking the world you'll see");
+        Console.WriteLine("They'll be calling me, calling me");
+        Console.WriteLine("They'll be calling me royalty");
+        Console.ResetColor();
+        Console.WriteLine();
+
+        Console.WriteLine("They'll be calling me royalty");
+        Console.WriteLine();
+
+        Console.WriteLine("They say I'm dangerous, 'cause I");
+        Console.WriteLine("Broke all the cages");
+        Console.WriteLine("No I, won't sit and take it");
+        Console.WriteLine("Now, now");
+        Console.WriteLine("They left me for dead, I guess they'll never learn");
+        Console.WriteLine("Every time I break there's just more pain to burn");
+        Console.WriteLine("They'll never, never, never learn");
+        Console.WriteLine();
+
+        Console.WriteLine("Oooh, oooh, oooh");
+        Console.WriteLine("Till the battles won");
+        Console.WriteLine("Till kingdom come");
+        Console.WriteLine("Oooh, oooh, oooh");
+        Console.WriteLine("I'll never run");
+        Console.WriteLine();
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Best to give me your loyalty");
+        Console.WriteLine("Cause I'm taking the world you'll see");
+        Console.WriteLine("They'll be calling me, calling me");
+        Console.WriteLine("They'll be calling me royalty");
+        Console.WriteLine("Best to give me your loyalty");
+        Console.WriteLine("Cause I'm taking the world you'll see");
+        Console.WriteLine("They'll be calling me, calling me");
+        Console.WriteLine("They'll be calling me royalty");
+        Console.ResetColor();
+        Console.WriteLine();
+
+        Console.WriteLine("They'll be calling me royalty");
+        Console.WriteLine("They'll be calling me royalty");
+        Console.WriteLine("They'll be calling me royalty");
